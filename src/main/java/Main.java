dd59a1e4c75bc6b280434dee2f4ceb8cbf3c2e71
alejandro <20160338@ce.pucmm.edu.do>
2020-12-23 00:14:30 -0400

    Preparando para actualizar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,24 +31,24 @@ public class Main {
         });
         app.start(8000);
         DBStart.getInstancia().init();
-//        Persona persoo = new Persona("Alejandro", "4021527","alejandrodelonc15@gmail.com", new Date(), "j amor",
-//                "admin", "admin", "sdfsasdf");
-//        Persona pi = new Persona("Raspberry Pi", "000000", "", new Date(), "La zursa",
-//                "pi", "raspberry", "ninguno");
-//        PersonaServices.getInstancia().editar(persoo);
-//        PersonaServices.getInstancia().editar(pi);
-//
-//        Format forma = new SimpleDateFormat("dd/MM/yyyy");
-//        Perro pers = new Perro("484849535648495350491310","Billy", new Date(), 2) ;
-//        PerroServices.getInstancia().crear(pers);
-//
-//        Plan pl = new Plan("Prueba", Float.parseFloat("870.89") , 3, 3);
-//        PlanServices.getInstancia().crear(pl);
-//
-//        Dispensador dispen = new Dispensador("0013A20040A4D103", "-70.5289368", "9.2209351","Calle 7");
-//        DispensadorServices.getInstancia().crear(dispen);
-//        HistorialDeVisitas vis = new HistorialDeVisitas(pers, dispen, new Date() , true);
-//        HistorialDeVisitasService.getInstancia().crear(vis);
+        Persona persoo = new Persona("Alejandro", "4021527","alejandrodelonc15@gmail.com", new Date(), "j amor",
+                "admin", "admin", "sdfsasdf");
+        Persona pi = new Persona("Raspberry Pi", "000000", "", new Date(), "La zursa",
+                "pi", "raspberry", "ninguno");
+        PersonaServices.getInstancia().editar(persoo);
+        PersonaServices.getInstancia().editar(pi);
+
+        Format forma = new SimpleDateFormat("dd/MM/yyyy");
+        Perro pers = new Perro("484849535648495350491310","Billy", new Date(), 2) ;
+        PerroServices.getInstancia().crear(pers);
+
+        Plan pl = new Plan("Prueba", Float.parseFloat("870.89") , 3, 3);
+        PlanServices.getInstancia().crear(pl);
+
+        Dispensador dispen = new Dispensador("0013A20040A4D103", "-70.5289368", "9.2209351","Calle 7");
+        DispensadorServices.getInstancia().crear(dispen);
+        HistorialDeVisitas vis = new HistorialDeVisitas(pers, dispen, new Date() , true);
+        HistorialDeVisitasService.getInstancia().crear(vis);
 
         new ApiControlador(app).aplicarRutas();
         /*
